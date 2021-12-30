2121d6dc2da903242cff96eac3cadd6cb708e1e7
SKirilov7 <88241989+SKirilov7@users.noreply.github.com>
2021-12-30 18:01:50 +0200

    Updating a task.

--- PHPFundamentals/TextProcessing/Exercise/03.php
+++ PHPFundamentals/TextProcessing/Exercise/03.php
@@ -1,10 +1,10 @@
 <?php
 $pathToFile = readline();
 $indexToStart = strrpos($pathToFile, "\\") + 1;
-
-$pathFileNameAndExtension = explode('.', substr($pathToFile, $indexToStart, strlen($pathToFile)- $indexToStart + 1));
-$fileName = $pathFileNameAndExtension[0];
-$extension = $pathFileNameAndExtension[1];
+$file = substr($pathToFile, $indexToStart, strlen($pathToFile)- $indexToStart + 1);
+$lastDotIndex = strrpos($file, '.')+1;
+$fileName = substr($file, 0, $lastDotIndex - 1);
+$extension = substr($file, $lastDotIndex);
 
 echo "File name: $fileName".PHP_EOL;
 echo "File extension: $extension";
